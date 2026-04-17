94e3e2fad90283226873eb5c480a9d2b368df027
Nikos Strafiotis <115450409+straf10@users.noreply.github.com>
2026-04-17 13:46:04 +0300

    Code cleaning

--- src/training/train_mlc_greek_bert.py
+++ src/training/train_mlc_greek_bert.py
@@ -215,7 +215,7 @@ def train(config: dict):
 
 
     # Build label names from training data directly
-    labels_path = "/mnt/redpro/home/aid26009/ELCardioCC/data/Train_Set_2026/labelset.txt"
+    labels_path = "data/raw/Train_Set_2026/labelset.txt"
 
     with open(labels_path, "r", encoding="utf-8") as f:
         label_names = [line.strip() for line in f if line.strip()]
