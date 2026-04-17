0f1473cc5b9d1534679c41f24bd99ae17fbd2e8c
Nikos Strafiotis <115450409+straf10@users.noreply.github.com>
2026-04-17 23:44:45 +0300

    Modified analysis scripts

--- src/analysis/label_analysis.py
+++ src/analysis/label_analysis.py
@@ -7,13 +7,13 @@ try:
     from ..evaluation.config_utils import load_config, get_cfg
     from ..evaluation.evaluator import evaluate_data
     from ..evaluation.io_utils import load_ground_truth
-    from .common import load_scores_bundle, derive_predictions, label_support_from_gt, ensure_output_dir, load_model_artifacts
+    from .common import load_model_artifacts, label_support_from_gt
     from .error_analysis import build_confusion_views, range_vs_specific_summary
 except ImportError:
     from src.evaluation.config_utils import load_config, get_cfg
     from src.evaluation.evaluator import evaluate_data
     from src.evaluation.io_utils import load_ground_truth
-    from src.analysis.common import load_scores_bundle, derive_predictions, label_support_from_gt, ensure_output_dir, load_model_artifacts
+    from src.analysis.common import load_model_artifacts, label_support_from_gt
     from src.analysis.error_analysis import build_confusion_views, range_vs_specific_summary
 
 
